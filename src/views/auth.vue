021bcfda2b891e146d56d05c14d38511ee27ccca
JaumeBlond <46705356+JaumeBlond@users.noreply.github.com>
2024-05-01 15:54:53 +0200

    Solve singup not sync with bbdd

--- src/views/auth.vue
+++ src/views/auth.vue
@@ -2,12 +2,17 @@
     <div class="flex justify-center items-center h-screen">
         <div class="w-full max-w-md">
             <h1 class="text-3xl font-bold mb-8">Authentication</h1>
-            <form v-if="!isSignUpComputed" @submit.prevent="userLogIn" class="mb-8">
+            <form v-if="recoveryBeenClicked && !isSignUpComputed" @submit.prevent="signUp" class="mb-8">
+                <input type="email" v-model="email" placeholder="Email" required class="input-field" />
+                <button type="submit" class="btn-primary">Recover password</button>
+            </form>
+            <form v-else-if="!isSignUpComputed" @submit.prevent="userLogIn" class="mb-8">
                 <input type="email" v-model="email" placeholder="Email" required class="input-field" />
                 <input type="password" v-model="password" placeholder="Password" required class="input-field" />
                 <button type="submit" class="btn-primary">Sign In</button>
             </form>
-            <form v-else @submit.prevent="signUp" class="mb-8">
+
+            <form v-else @submit.prevent="createNewUser" class="mb-8">
                 <input type="text" v-model="name" placeholder="Name" required class="input-field" />
                 <input type="text" v-model="surname" placeholder="Surname" required class="input-field" />
                 <input type="email" v-model="email" placeholder="Email" required class="input-field" />
@@ -15,8 +20,8 @@
                 <button type="submit" class="btn-primary">Sign Up</button>
             </form>
             <div class="flex justify-between">
-                <button @click="recoverPassword" v-if="!isSignUpComputed" class="text-gray-600">Forgot
-                    Password?</button>
+                <button v-if="!isSignUpComputed" @click="ToggleRecoverPassword"
+                    class="text-gray-600">{{ recoveryBeenClicked ? 'Back to Sign In' : "Recover Password" }}</button>
                 <button @click="toggleSignUp" class="text-gray-600">{{ isSignUpComputed ? 'Back to Sign In' : "Create Account" }}</button>
             </div>
         </div>
@@ -27,19 +32,19 @@
 import { ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
 import { useUserStore } from '@/stores/userStore';
-import { supabase } from "@/api/supabase";
-import { logIn } from "@/api/usersApi";
 
 const router = useRouter();
 const userStore = useUserStore();
 
-const { signIn } = useUserStore()
+const { signIn, signUp, signOut } = useUserStore()
 
 let email = ref('');
 let password = ref('');
 let name = ref('');
 let surname = ref('');
-let error = null;
+const isSignUpComputed = ref(false);
+const recoveryBeenClicked = ref(false);
+
 
 watch(
     () => userStore.user,
@@ -54,24 +59,16 @@ const userLogIn = () => {
     signIn(email.value, password.value);
 };
 
-const signUp = async () => {
-    try {
-        const { user, error } = await supabase.auth.signUp({
-            email: email.value,
-            password: password.value,
-        });
-        if (error) {
-            throw error;
-        }
-        console.log(user);
-        router.push({ name: 'board' }); // Redirect to taskboard upon successful sign-up
-    } catch (error) {
-        error = error;
-    }
+const createNewUser = async () => {
+    signUp(email.value, password.value);
 };
 
 const recoverPassword = () => {
     console.log('Password recovery clicked');
+};
+
+const ToggleRecoverPassword = () => {
+    recoveryBeenClicked.value = !recoveryBeenClicked.value;
 };
 
 const toggleSignUp = () => {
@@ -79,7 +76,6 @@ const toggleSignUp = () => {
 };
 
 // Define isSignUpComputed as a ref
-const isSignUpComputed = ref(false);
 </script>
 
 <style scoped>
